feb1595438739d405fc51b57386ad0e1799da5df
Andy Lin <andyhsuanlin@gmail.com>
2025-07-09 17:28:07 +0800

    add title for prize picker

--- src/app/components/prizePicker.tsx
+++ src/app/components/prizePicker.tsx
@@ -25,14 +25,17 @@ export const PrizePicker = ({ prizeList, index, setIndex }: { prizeList: prize[]
 
 
 	return (
-		<div className="flex flex-row h-16 w-1/4 mx-auto">
-			<button className={`h-full ${prizeList.length === 0 && 'disabled'}`} onClick={dec}><Image src='/chevron-left.svg' alt="previous prize" height={32} width={32}></Image></button>
-			<div className={` overflow-x-hidden w-full `}>
-				<div className={`w-full flex flex-row transition-transform carousel-items`} style={{ '--index': -index * 100 + '%' } as React.CSSProperties}>
-					{prizeList.length ? prizeNames : emptyList()}
+		<div className="w-full p-4">
+			<h2 className="text-2xl font-bold text-center mt-4">當前獎項</h2>
+			<div className="flex flex-row h-16 w-1/4 mx-auto">
+				<button className={`h-full ${prizeList.length === 0 && 'disabled'}`} onClick={dec}><Image src='/chevron-left.svg' alt="previous prize" height={32} width={32}></Image></button>
+				<div className={` overflow-x-hidden w-full `}>
+					<div className={`w-full flex flex-row transition-transform carousel-items`} style={{ '--index': -index * 100 + '%' } as React.CSSProperties}>
+						{prizeList.length ? prizeNames : emptyList()}
+					</div>
 				</div>
+				<button className={`${prizeList.length === 0 && 'disabled'}`} onClick={inc}><Image src='/chevron-right.svg' alt="next prize" height={32} width={32}></Image></button>
 			</div>
-			<button className={`${prizeList.length === 0 && 'disabled'}`} onClick={inc}><Image src='/chevron-right.svg' alt="next prize" height={32} width={32}></Image></button>
 		</div>
 	)
 }
