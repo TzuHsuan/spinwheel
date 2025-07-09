e040d901eb53b32e2273c927f7e9e59b88b4022c
Andy Lin <andyhsuanlin@gmail.com>
2025-07-09 16:39:46 +0800

    remove unused import

---
+++ src/app/components/prizePicker.tsx
@@ -0,0 +1,38 @@
+import type { Dispatch, SetStateAction } from "react"
+import Image from "next/image";
+
+import { prize } from "./prizes";
+
+export const PrizePicker = ({ prizeList, index, setIndex }: { prizeList: prize[], index: number, setIndex: Dispatch<SetStateAction<number>> }) => {
+
+	const prizeNames = prizeList.map((prize, i) => {
+		return (<div className={`m-auto ${index === i ? '' : 'opacity-50'} min-w-full text-center`} key={prize.id}>{prize.id}</div>)
+	})
+
+
+	const emptyList = () => { return (<div className={`m-auto transition-opacity  min-w-full text-center`} >No Items Available</div>) }
+
+	const dec = () => {
+		setIndex(i => {
+			i -= 1
+			return i < 0 ? prizeList.length - 1 : i
+		})
+	}
+
+	const inc = () => {
+		setIndex(i => (i + 1) % prizeList.length)
+	}
+
+
+	return (
+		<div className="flex flex-row h-16 w-1/4 mx-auto">
+			<button className={`h-full ${prizeList.length === 0 && 'disabled'}`} onClick={dec}><Image src='/chevron-left.svg' alt="previous prize" height={32} width={32}></Image></button>
+			<div className={` overflow-x-hidden w-full `}>
+				<div className={`w-full flex flex-row transition-transform carousel-items`} style={{ '--index': -index * 100 + '%' } as React.CSSProperties}>
+					{prizeList.length ? prizeNames : emptyList()}
+				</div>
+			</div>
+			<button className={`${prizeList.length === 0 && 'disabled'}`} onClick={inc}><Image src='/chevron-right.svg' alt="next prize" height={32} width={32}></Image></button>
+		</div>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect, ReactElement } from "react"
+import React, { useState, useEffect } from "react"
 import { Tracks } from "@/app/components/tracks"
 import { Visualizer } from "./components/eq"
 import { Roulette } from "./components/roulette"
