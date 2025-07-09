21970cc0bdd6b169c83af61bc64087b09cbab59f
Andy Lin <andyhsuanlin@gmail.com>
2025-07-09 22:28:10 +0800

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+
+  return (<div className="bg-background-300 text-black p-4 h-full">
+    <h1 className="text-xl m-4">Copyright Disclaimer</h1>
+
+    <p className="mx-4">All music tracks featured in this project are used for illustrative, educational, and/or non-commercial purposes only. I do not claim ownership of any musical compositions, recordings, or performances included herein. All rights to the underlying music remain with their respective copyright holders.</p>
+    <ul className="mx-4 mt-4 list-disc">
+
+      <li>This project is strictly non-profit and is not intended for commercial distribution.</li>
+      <li>No copyright infringement is intended.</li>
+      <li>All trademarks, service marks, and trade names are the property of their respective owners.</li>
+      <li>If you are the copyright owner of any music used here and believe use of your work violates copyright law, please contact me at zuphest@gmail.com so we can discuss appropriate attribution, licensing, or removal.</li>
+    </ul>
+  </div>)
+}
