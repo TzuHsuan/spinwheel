340b22bac3d4eb5fc7702b43a5fa0aad8df4aef4
Andy Lin <andyhsuanlin@gmail.com>
2025-07-09 16:39:28 +0800

    fix "jump" in side bar collapse animation

--- src/app/components/side.tsx
+++ src/app/components/side.tsx
@@ -15,10 +15,10 @@ export const Side = ({ blocks }: { blocks: panelBlock[] }) => {
 				{blocks.map((child, index: number) => (
 					<div key={index} className={`flex flex-col items-center w-fit relative cursor-pointer ${activeChild === index ? 'z-2' : 'z-1'} bg-gray-800 hover:bg-gray-600 transition-colors duration-200`}>
 						<div onClick={() => activeChild === index ? setActiveChild(null) : setActiveChild(index)} className={`${activeChild === index ? 'w-md' : 'w-fit'} p-4 flex flex-row align-center`}>
-							<p className={`text-center ${activeChild === index ? 'w-full' : 'w-0'} h-8 overflow-hidden transition-[width]`} >{child.title}</p>
+							<p className={`text-center ${activeChild === index ? 'w-full' : 'w-0'} h-8 overflow-hidden `} >{child.title}</p>
 							<img src={child.icon} alt={`Icon ${child.title}`} className={`w-8 h-8  `} />
 						</div>
-						<div className={`${(activeChild !== index) && 'scale-x-0'} absolute left-0 top-16 w-md overflow-hidden p-2 bg-gray-700 rounded-md text-white origin-left`}>
+						<div className={`${(activeChild !== index) ? 'scale-x-0' : 'scale-x-100'} absolute left-0 top-16 w-md overflow-hidden p-2 bg-gray-700 rounded-md text-white origin-left transition-transform`}>
 							<child.content {...child.props} />
 						</div>
 
