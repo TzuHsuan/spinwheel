e4e9f7539dc5aa42693b1abe5867f05ac20f8b28
Andy Lin <andyhsuanlin@gmail.com>
2025-07-10 17:10:56 +0800

    fix basepath in config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: '/spinwheel',
-  publicRuntimeConfig: { basePath: '/spinwheel' },
   assetPrefix: '/spinwheel',
   /* config options here */
 };
